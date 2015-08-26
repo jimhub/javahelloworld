41be4e5b2cd8e99ffa231c1543778c3db8fca8c6
James Liljenquist <jliljenq@gmail.com>
2015-08-26 23:15:05 +0000

    Meow

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,5 +2,6 @@ public class JavaHelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello World from Jaba");
 		System.out.println("Hello again");
+		System.out.println("HERE'S A NEW LINE");
 	}
 }
